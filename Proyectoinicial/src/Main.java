6eb0b045feb4bea16e3a06843c568ca8e91395ad
Andrei Friz <frizandrei19102007@gmail.com>
2025-10-02 19:54:48 +0200

    update

--- Proyectoinicial/src/Main.java
+++ Proyectoinicial/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     //2. Metodo. Definen funcionalidades. Tienen argumentos, retorno o void, y cuerpo
     //metodo Main. Metodo principal del programa. La puerta de entrada del programa. 1 por programa
     public static void main(String[] args){
-        //TODO "(String[] args)" es necesario solo para el jdk inferior al 25
+        // "(String[] args)" es necesario solo para el jdk inferior al 25
         System.out.println("Este es mi primer programa en Java");
         //println -> con salto, print -> sin salto
         System.out.println("Esta es la segunda linea");

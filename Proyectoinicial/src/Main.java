2e3b93a4caf1d8200a9633b1190f5980f5d848b8
Andrei Friz <frizandrei19102007@gmail.com>
2025-10-02 19:50:00 +0200

    update

---
+++ Proyectoinicial/src/Main.java
@@ -0,0 +1,17 @@
+//1. Clase. Estructura basica de la programacion donde escribimos nuestro codigo. En POO es el molde de un objeto
+public class Main {
+    //2. Metodo. Definen funcionalidades. Tienen argumentos, retorno o void, y cuerpo
+    //metodo Main. Metodo principal del programa. La puerta de entrada del programa. 1 por programa
+    public static void main(String[] args){
+        // TODO "(String[] args)" es necesario solo para el jdk inferior al 25
+        System.out.println("Este es mi primer programa en Java");
+        //println -> con salto, print -> sin salto
+        System.out.println("Esta es la segunda linea");
+        
+    }
+    //firma del metodo
+    public void nombreMetodo() {
+        //argumento Main. Metodo principal del programa. La puerta de entrada del programa. 1 por programa
+
+    }
+}   
